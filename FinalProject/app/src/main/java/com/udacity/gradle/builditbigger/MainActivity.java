85f1bb820fb6d6b0f72a08c9987dab329a5612fc
buntoro tandjaja <wen_college@yahoo.com>
2018-10-16 14:50:05 -0700

    flavor works, ad missing

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,23 +1,29 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.btandjaja.www.displayjoke.DisplayJoke;
+import com.google.android.gms.ads.AdView;
 
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse{
     private EndpointAsyncTask endpointAsyncTask;
-    private String mJoke;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAdView = findViewById(R.id.adView);
+        String flavor = BuildConfig.PAID_FLAVOR ? "PAID": "FREE";
+        Toast.makeText(this, flavor, Toast.LENGTH_LONG).show();
     }
 
 
