a1a9a19282468aa4d009a9eab7ac5bee449ce3ac
buntoro tandjaja <wen_college@yahoo.com>
2018-10-20 22:19:18 -0700

    update asynctask test unit

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 import static junit.framework.TestCase.assertNotNull;
@@ -20,9 +28,11 @@ public class AsyncTaskTest {
     public ActivityTestRule<MainActivity> myActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     Context context;
+    private MyApi myApiService;
+    private String TAG = AsyncTaskTest.class.getSimpleName();
 
     @Test
-    public void testAsynctask() throws InterruptedException{
+    public void testAsynctask() throws InterruptedException {
         assertTrue(true);
         final CountDownLatch latch = new CountDownLatch(1);
         context = InstrumentationRegistry.getContext();
@@ -33,6 +43,31 @@ public class AsyncTaskTest {
                 if (result != null) {
                     assertTrue(result.length() > 0);
                     latch.countDown();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Context... params) {
+                if (myApiService == null) {  // Only do this once
+                    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                            new AndroidJsonFactory(), null)
+                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                            .setApplicationName("FinalProject")
+                            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                                @Override
+                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                    abstractGoogleClientRequest.setDisableGZipContent(true);
+                                }
+                            });
+                    // end options for devappserver
+                    myApiService = builder.build();
+                }
+
+                try {
+                    return myApiService.retrieveJoke("a").execute().getJoke();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage());
+                    return null;
                 }
             }
         };
